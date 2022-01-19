e061f09a914d38ce56b8abe151988d1d2a6ee547
Henousek <60482905+HenyPotter@users.noreply.github.com>
2022-01-19 22:49:52 +0100

    Create smp_notifier.py

---
+++ smp_notifier.py
@@ -0,0 +1,16 @@
+import requests
+from discord_webhook import DiscordWebhook, DiscordEmbed
+
+
+webhook = DiscordWebhook(url='WEBHOOK URL TOP SECRET KIDE')
+
+embed = DiscordEmbed(title='<a:alarm:905101481785561229> Výpadek TrospySMP <a:alarm:905101481785561229>', description='Pracujeme na opravě! <a:pepeHacker:933465128328888361> ', color='FF0000')
+embed.set_timestamp()
+webhook.add_embed(embed)
+
+r = requests.get('https://trospysmp.wtf/')
+print("r.status_code")
+
+if r.status_code == 404 :
+    print("Code 404")
+    response = webhook.execute()
